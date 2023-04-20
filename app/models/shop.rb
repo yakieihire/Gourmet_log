59f24876412b03235b6c71edbd5d55003836b4be
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-20 22:58:04 +0900

    active_hash実装

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,7 @@
 class Shop < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+
+  validates :store_name, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
