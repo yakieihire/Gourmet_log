b3d69f30db6c643c002b7b8723cb133148a0bf3a
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-20 23:20:09 +0900

    コントローラー記述追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,7 @@
 class Shop < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  has_one_attached :image
   belongs_to :user
 
   validates :store_name, presence: true
