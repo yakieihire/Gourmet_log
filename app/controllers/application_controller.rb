c798cdd57ffb8344b106252809a01d80242883ca
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-20 22:40:09 +0900

    バリデーション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  def index
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 end
