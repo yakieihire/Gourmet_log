07024dad819ac36058a5624914f6fe35b9d9b190
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-20 22:47:19 +0900

    モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_20_131631) do
+ActiveRecord::Schema.define(version: 2023_04_20_134544) do
+
+  create_table "shops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "store_name", null: false
+    t.integer "category_id", null: false
+    t.string "menu", null: false
+    t.string "place", null: false
+    t.text "store_url", null: false
+    t.text "memo", null: false
+    t.integer "interesting_store_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shops_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +39,5 @@ ActiveRecord::Schema.define(version: 2023_04_20_131631) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shops", "users"
 end
