9bb196df8d9e74b330a11410b323b1f143e76936
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-21 00:48:02 +0900

    コントローラー修正

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,10 +1,11 @@
 class ShopsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
     @shops = Shop.order('created_at DESC')
   end
 
   def new
-    @Shop = Shop.new
+    @shop = Shop.new
   end
 
   def create
@@ -17,15 +18,12 @@ class ShopsController < ApplicationController
   end
 
   def show
-    @shop = Shop.find(params[:id])
   end
 
   def edit
-    @shop = Shop.find(params[:id])
   end
 
   def update
-    @shop = Shop.find(params[:id])
     if @shop.update(shop_params)
       redirect_to shop_path(@shop.id)
     else
@@ -33,8 +31,21 @@ class ShopsController < ApplicationController
     end
   end
 
+  def destroy
+    if @shop.destroy
+      redirect_to root_path
+    else
+    render :index
+    end
+  end
+
+
   private
   def shop_params
     params.require(:shop).permit(:image, :store_name, :category_id, :menu, :place, :store_url, :memo, :interesting_store_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @shop = Shop.find(params[:id])
   end
 end
