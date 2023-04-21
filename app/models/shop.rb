bd1b47126a16442f7696d065a9f0a978adf89fb8
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-21 16:11:30 +0900

    バリデーション修正

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,10 +1,10 @@
 class Shop < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  has_one_attached :image
+  has_many_attached :images
   belongs_to :user
 
-  validates :image, presence: true
+  validates :images, presence: true
   validates :store_name, presence: true
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
