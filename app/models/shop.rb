33135e5e432cb4985c40a87bc190e12ce5340ef6
yakieihire <127078696+yakieihire@users.noreply.github.com>
2023-04-24 17:18:28 +0900

    バリデーション修正

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,9 +5,9 @@ class Shop < ApplicationRecord
   belongs_to :user
 
   validates :images, presence: true
-  validates :images, length: { minimum: 1, maximum: 4, message: "は1枚以上5枚以下にしてください" }
+  validates :images, length: { minimum: 1, maximum: 4, message: "は1枚以上4枚以下にしてください" }
   validates :store_name, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
 
   def self.search(search)
     if search.present?
